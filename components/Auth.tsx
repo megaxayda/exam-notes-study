fe5a5f2d6418ffbd2789698d540ce538d30bb5e5
Tai Pham <megaxayda@gmail.com>
2022-06-05 11:27:24 +0700

    update

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -23,7 +23,7 @@ export default function Auth() {
       <div className="col-6 form-widget">
         <h1 className="header">Supabase + Next.js</h1>
         <p className="description">
-          Sign in via magic link with your email below
+          Sign in via magic link with your email below asd asd asd
         </p>
         <div>
           <input
